218b9a3e7a07809cc0b1a524b722dfd3ebb174a1
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-31 18:17:34 +0100

    check_online

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
+use std::fs::read_to_string;
 use std::net::TcpStream;
-
+struct Port {
+    name: String,
+    port: u16,
+}
+struct Ip {
+    ip: String,
+    ports: Vec<Port>,
+}
 fn main() -> std::io::Result<()> {
-    if let Ok(stream) = TcpStream::connect("1.1.1.1:80") {
+    println!("online: {}", check_online("1.1.1.1", 80));
+    read_json();
+    Ok(())
+}
+fn check_online(ip: &str, port: u16) -> bool {
+    if let Ok(stream) = TcpStream::connect(format!("{}:{}", ip, port)) {
         println!("Connected to the server! {:?}", stream);
+        return true;
     } else {
         println!("Couldn't connect to server...");
     };
-    Ok(())
+    false
+}
+fn read_json() {
+    println!("{}", read_to_string("./ips.json").expect("error"));
 }
